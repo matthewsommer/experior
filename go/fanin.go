b1d17a1cf30ae2e1b503bef22e55f8874977d554
matthewsommer <matthew.sommer@gmail.com>
2018-04-23 20:59:21 -0700

    fanin example and changed file names

---
+++ go/fanin.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+func main() {
+	c := fanIn(boring("Joe"), boring("Ann"))
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
+	}
+	fmt.Println("You're boring; I'm leaving.")
+}
+
+func boring(msg string) <- chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(4e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() { for { c <- <-input1 } }()
+	go func() { for { c <- <-input2 } }()
+	return c
+}
